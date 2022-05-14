2b6d351ba4d9517d42d4b85f197b55b3e19b4ea1
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-05-14 11:00:36 +0000

    add breed model

--- woofme_app/models.py
+++ woofme_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+import numpy as np
 
 # Create your models here.
+class Breed(models.Model):
+    breed_name = models.Charfield(max_length=200)
+
+    def average_review(self):
+        all_reviews = map(lambda x: x.rating, self.review_set.all())
+        return np.mean(all_reviews)
+
+    def __unicode__(self):
+        return self.name
+
+        
