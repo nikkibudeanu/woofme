5c7815a74371206e70adb18d67b0de692c47248e
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-05-14 11:15:38 +0000

    add configuration to the admin panel

--- woofme_app/admin.py
+++ woofme_app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Breed, BreedReview
 
 # Register your models here.
+
+class BreedReviewAdmin(admin.ModelAdmin):
+    model = BreedReview
+    list_display = ('breed', 'user_name', 'review', 'published_date')
+    list_filter = ['published_date', 'user_name']
+    search_fields = ['breed','review']
+
+admin.site.register(BreedReview, BreedReviewAdmin)
