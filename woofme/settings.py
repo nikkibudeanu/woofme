df2d2bd56dccbe0c23c98e1c8c9e0196d63328d2
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-05-16 17:29:15 +0000

    create register user template

--- woofme/settings.py
+++ woofme/settings.py
@@ -85,14 +85,16 @@ WSGI_APPLICATION = 'woofme.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
-DATABASES = {
+if DEBUG:
+    DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': os.path.join(BASE_DIR / 'db.sqlite3'),
+     }
+}
+else:
+    DATABASES = {
     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
