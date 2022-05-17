fc861e09916d60ab2a2c610b63db9f5b518d1566
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-05-17 18:21:51 +0000

    refactor models and forms

--- woofme_app/urls.py
+++ woofme_app/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path, include
-from . import views 
+from .views import HomeView, BreedRatingView, AddReviewView
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('add_review', views.add_review, name = "add_review"),
-    path('review_list/', views.review_list, name = "review_list"),
-    path('review_list/pk:<review_id>', views.review_page, name='review_page'),
-    path('breed', views.breed_list, name='breed_list'),
-    path('breed/pk:<breed_id>', views.breed_page, name='breed_page'),
+    path('', HomeView.as_view(), name='home'),
+    path('rate/<int:pk>', BreedRatingView.as_view(), name = "breed_rate"),
+    path('add_review', AddReviewView.as_view(), name = "add_review"),
+    #path('review_list/pk:<review_id>', views.review_page, name='review_page'),
+    #path('breed', views.breed_list, name='breed_list'),
+    #path('breed/pk:<breed_id>', views.breed_page, name='breed_page'),
 
 ]
--- woofme_app/views.py
+++ woofme_app/views.py
@@ -1,37 +1,47 @@
 from django.shortcuts import render, get_object_or_404
 from .models import BreedReview, Breed
 from .forms import BreedReviewForm
+from django.views.generic import ListView, DetailView, CreateView
+
 
 # Create your views here.
-def home(request):
-    return render(request, 'woofme_app/home.html', {})
+class HomeView(ListView):
+    model = BreedReview
+    template_name = 'home.html'
 
 
-def add_review(request):
-    form = BreedReviewForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-    context = {
-        'form':form
-    }
 
-    return render(request, 'woofme_app/add_review.html', context)
+class AddReviewView(CreateView):
+    model = BreedReview
+    template_name = 'add_review.html'
+    fields = '__all__'
+    def get_absolute_url(self):
+        return reverse('home')
 
 
-def review_list(request):
-    review_list = BreedReview.objects.order_by('-published_date')[:9]
-    context = {'review_list':review_list}
-    return render(request, 'woofme_app/review_list.html', context)
+class BreedRatingView(DetailView):
+    model = BreedReview
+    queryset = BreedReview.objects.order_by("-published_date")
+    template_name = 'review_list.html'
 
-def review_page(request, review_id):
-    review = get_object_or_404(BreedReview, pk=review_id)
-    return render(request, 'woofme_app/review_page.html', {'review':review})
 
-def breed_list(request):
-    breed_list = Breed.objects.order_by('-name')
-    context = {'breed_list':breed_list}
-    return render(request, 'woofme_app/breed_list.html', context)
+# def review_list(request):
+#     review_list = BreedReview.objects.order_by('-pub_date')[:9]
+#     context = {'review_list':review_list}
+#     return render(request, 'review_list.html', context)
 
-def breed_page(request, breed_id):
-    breed = get_object_or_404(Breed, pk=breed_id)
-    return render(request, 'woofme_app/breed_page.html',{'breed':breed})
+
+# def review_page(request, review_id):
+#     review = get_object_or_404(BreedReview, pk=review_id)
+#     return render(request, 'reviews/review_list.html', {'review': review})
+
+
+# def breed_list(request):
+#     breed_list = Breed.objects.order_by('-name')
+#     context = {'breed_list':breed_list}
+#     return render(request, 'reviews/breed_list.html', context)
+
+
+# def breed_page(request, breed_id):
+#     breed = get_object_or_404(Breed, pk=breed_id)
+#     return render(request, 'reviews/breed_detail.html', {'breed': breed})
