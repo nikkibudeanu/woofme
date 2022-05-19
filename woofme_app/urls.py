bd92edd079a3748fd5e0a02aad7b7407ecca2ef5
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-05-19 11:28:29 +0000

    uncomment urls

--- woofme_app/urls.py
+++ woofme_app/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('rate/<int:pk>', BreedRatingView.as_view(), name = "breed_rate"),
     path('add_review', AddReviewView.as_view(), name = "add_review"),
-    #path('review_list/pk:<review_id>', views.review_page, name='review_page'),
-    #path('breed', views.breed_list, name='breed_list'),
-    #path('breed/pk:<breed_id>', views.breed_page, name='breed_page'),
+    path('review_list/pk:<review_id>', views.review_page, name='review_page'),
+    path('breed', views.breed_list, name='breed_list'),
+    path('breed/pk:<breed_id>', views.breed_page, name='breed_page'),
 
 ]
