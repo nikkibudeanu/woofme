3a9d5e53fec2020d9460b72cb690ec319b861575
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-05-23 19:37:28 +0000

    test search bar

--- woofme_app/models.py
+++ woofme_app/models.py
@@ -82,7 +82,7 @@ class BreedReview(models.Model):
     slug = models.CharField(max_length=50, null=True)
     breed_image = CloudinaryField('image', blank=True)
     published_date = models.DateTimeField(auto_now_add=True)
-    user_name = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user_name = models.ForeignKey(User, on_delete=models.CASCADE)
     review = models.CharField(max_length=200)
     adaptability = models.IntegerField(choices=ADAPTABILITY_CHOICES)
     friendliness = models.IntegerField(choices=FRIENDLINESS_CHOICES)
