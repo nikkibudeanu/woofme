6e27c6fa1790adca0504e1ddb2a59f761f1ef51d
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-05-26 14:50:22 +0000

    refactor breed group, breed and breed reviews models

--- woofme_app/models.py
+++ woofme_app/models.py
@@ -2,19 +2,21 @@ from django.urls import reverse
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
-import numpy as np
-from django.conf import settings 
+from django.conf import settings
 
 # Create your models here.
 
 
 class BreedGroup(models.Model):
     """ Create a breed group model form """
-    breed_group = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         """ Return breed group name string """
-        return str(self.breed_group).lower()
+        return str(self.name).lower()
 
     def get_absolute_url(self):
         """ Redirect user to add review page"""
@@ -23,12 +25,16 @@ class BreedGroup(models.Model):
 
 class Breed(models.Model):
     """ Create a breed model form"""
-    breed_name = models.CharField(max_length=200, unique=True)
-    group = models.ManyToManyField(BreedGroup)
+    name = models.CharField(max_length=200, unique=True)
+    breed_group = models.ForeignKey(BreedGroup, on_delete=models.CASCADE, null=True)
+    slug = models.CharField(max_length=50, null=True)
+    breed_image = CloudinaryField('image', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         """ Return breed name string """
-        return str(self.breed_name).lower()
+        return str(self.name).lower()
 
     def get_absolute_url(self):
         """ Redirect user to add review """
@@ -37,38 +43,6 @@ class Breed(models.Model):
 
 class BreedReview(models.Model):
     """ Create a breed review model form"""
-    ADAPTABILITY_CHOICES = (
-        (1, '1'),
-        (2, '2'),
-        (3, '3'),
-        (4, '4'),
-        (5, '5'),
-    )
-
-    FRIENDLINESS_CHOICES = (
-        (1, '1'),
-        (2, '2'),
-        (3, '3'),
-        (4, '4'),
-        (5, '5'),
-    )
-
-    TRAINABILITY_CHOICES = (
-        (1, '1'),
-        (2, '2'),
-        (3, '3'),
-        (4, '4'),
-        (5, '5'),
-    )
-
-    HEALTH_GROOMING_NEEDS_CHOICES = (
-        (1, '1'),
-        (2, '2'),
-        (3, '3'),
-        (4, '4'),
-        (5, '5'),
-    )
-
     RATING_CHOICES = (
         (1, '1'),
         (2, '2'),
@@ -76,25 +50,19 @@ class BreedReview(models.Model):
         (4, '4'),
         (5, '5'),
     )
-
-    breed_group = models.ForeignKey(BreedGroup, on_delete=models.CASCADE, null=True)
     breed = models.ForeignKey(Breed, on_delete=models.CASCADE)
-    slug = models.CharField(max_length=50, null=True)
-    breed_image = CloudinaryField('image', blank=True)
-    published_date = models.DateTimeField(auto_now_add=True)
-    user_name = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     review = models.TextField(max_length=200)
-    adaptability = models.IntegerField(choices=ADAPTABILITY_CHOICES, default=0)
-    friendliness = models.IntegerField(choices=FRIENDLINESS_CHOICES, default=0)
-    trainability = models.IntegerField(choices=TRAINABILITY_CHOICES, default=0)
-    health_and_grooming_needs = models.IntegerField(choices=HEALTH_GROOMING_NEEDS_CHOICES, default=0)
-    rating = models.IntegerField(default=0)
+    adaptability = models.IntegerField(choices=RATING_CHOICES, default=0)
+    friendliness = models.IntegerField(choices=RATING_CHOICES, default=0)
+    trainability = models.IntegerField(choices=RATING_CHOICES, default=0)
+    health_and_grooming_needs = models.IntegerField(choices=RATING_CHOICES, default=0)
+    rating = models.IntegerField(choices=RATING_CHOICES, default=0)
 
     def __str__(self):
-        return str(self.breed) + '|' + str(self.user_name)
+        return str(self.breed) + '|' + str(self.author)
 
     def get_absolute_url(self):
         return reverse('review_list', kwargs={'pk': self.pk})
-
-
-breed_review = BreedReview.objects.all()
