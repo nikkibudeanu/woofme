56a4d8a959f1afa6a43f5fda4a52a9c82272ac0e
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-05-29 17:38:10 +0000

    solve errors on test forms file

--- woofme_app/test_forms.py
+++ woofme_app/test_forms.py
@@ -1,28 +1,37 @@
 from django.test import TestCase
+from .models import Breed, BreedGroup
 from .forms import CreateBreedForm
+
+
+class SetupClass(TestCase):
+    
+    def set_up(self):
+        self.breed = Breed.objects.create(breed_name='nametest')
+        self.breed_group = BreedGroup.objects.create(breed_group='grouptest')
 
 
 class CreateBreedFormTest(TestCase):
     """ Test create breed form """
-    def test_createbeer_form_valid(self):
+   
+    def test_create_breed_form_valid(self):
         """Test if breed form is valid"""
         form = CreateBreedForm(data={
-            "breed_name": "nametest1"
+            "name": "nametest1"
         })
         self.assertTrue(form.is_valid())
 
-    def test_createbreed_form_is_not_valid(self):
+    def test_create_breed_form_is_not_valid(self):
         """ Test if Create Breed Form  is invalid"""
         form = CreateBreedForm(data={
-            "breed_name": ""
+            "name": ""
         })
         self.assertFalse(form.is_valid())
 
     def test_create_breed_form_length(self):
 
         form = CreateBreedForm(data={
-            "breed_name" : str('a'*300)
+            "name": str('a'*300)
         })
-        self.assertFalse(form.is_valid)
+        self.assertFalse(form.is_valid())
 
 
--- woofme_app/test_urls.py
+++ woofme_app/test_urls.py
@@ -1,14 +1,14 @@
 from django.test import TestCase
-from django.urls import reverse, resolve 
 
 
-class test_urls(TestCase):
-
-    def test_home_page_working(self):
-        response = self.client.get('/')
-        self.assertEquals(response.status_code, 200)
+class TestUrls(TestCase):
     
+    def test_home_page_working(self):
+        response = self.client.get('')
+        self.assertEqual(response.status_code, 200)
+      
+      
     def test_add_review_page_working(self):
         response = self.client.get('/add_review')
-        self.assertEquals(response.status_code, 200)
+        self.assertEqual(response.status_code, 301)
 
