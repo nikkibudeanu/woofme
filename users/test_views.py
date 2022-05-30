63000ffede68ff0a9a3d40c6ec4a948295940390
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-05-30 20:29:56 +0000

    add login tests

--- users/test_views.py
+++ users/test_views.py
@@ -26,3 +26,33 @@ class TestRegister(TestCase):
         def test_register_user_get(self):
             response = self.client.get(reverse('register'))
             self.assertEqual(response.status_code, 200)
+    
+    class TestLogin(TestCase):
+        def setUp(self):
+            self.user = User.objects.create_user(
+                username='Aiokdn',
+                email='aiokdn@gmail.com',
+                password='12345')
+
+        def test_if_user_can_login(self):
+            response = self.client.post(reverse(
+                'login'),{
+                    "usename": "aiokdn",
+                    "password":"12345"
+                })
+            self.assertRedirects(response, '/',
+                status_code=302,
+                target_status_code=200,
+                fetch_redirect_response=True)
+
+        def test_if_user_cannot_login(self):
+            response = self.client.post(reverse(
+                'login'),{
+                    "username": "",
+                    "password": "12345"
+                })
+            self.assertRedirects(
+                response, '/users/login_user',
+                status_code=302,
+                target_status_code=200,
+                fetch_redirect_response=True)
