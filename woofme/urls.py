dfe1aafde8ad058157c047c717494cc99197b1e9
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-06-26 19:06:31 +0000

    solve typerror to display review list page

--- woofme/urls.py
+++ woofme/urls.py
@@ -6,6 +6,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('woofme_app.urls'), name="woofme_app_urls"),
+    path('', include('woofme_app.urls')),
     path('users/', include('users.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
