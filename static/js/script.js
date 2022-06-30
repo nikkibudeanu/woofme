63ad5ea2c1e520d35946ca59449f16586555e6fa
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-06-30 19:49:12 +0000

    refactor add review page

--- static/js/script.js
+++ static/js/script.js
@@ -3,6 +3,6 @@ var info_messages = document.getElementsByClassName('alert');
 setTimeout(function(){
     for (var i = 0; i < info_messages.length; i ++) {
         // Set display attribute as !important, neccessary when using bootstrap
-        info_messages[i].setAttribute('style', 'display:none !important');
+        info_messages[i].setAttribute('group', 'display:none !important');
     }
 }, 5000); 
