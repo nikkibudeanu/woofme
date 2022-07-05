64b2d29d144d3da827ec98444649a949ea3ebcdb
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-07-05 21:39:24 +0000

    check coverage reports

--- woofme/settings.py
+++ woofme/settings.py
@@ -58,11 +58,21 @@ INSTALLED_APPS = [
     'bootstrap_modal_forms',
     'active_link',
     'rest_framework',
+    'django_nose',
 
 
     # apps
     'woofme_app',
     'users',
+]
+
+# Use nose to run all tests
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+
+# Tell nose to measure coverage on the 'foo' and 'bar' apps
+NOSE_ARGS = [
+    '--with-coverage',
+    '--cover-package=foo,bar',
 ]
 
 
