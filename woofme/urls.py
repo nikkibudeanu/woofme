a09e81d319e6c5526c19d9f7e4dc3192590df3b1
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-07-06 21:29:39 +0000

    Add urls to error pages

--- woofme/urls.py
+++ woofme/urls.py
@@ -9,3 +9,9 @@ urlpatterns = [
     path('', include('woofme_app.urls')),
     path('users/', include('users.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+handler404 = "woofme.views.page_not_found"
+handler500 = "woofme.views.custom_error"
+handler400 = "woofme.views.bad_request"
+
--- woofme/views.py
+++ woofme/views.py
@@ -2,17 +2,16 @@
 from django.shortcuts import render
 
 
-def page_not_found_view(request, exception):
+def page_not_found(request, exception):
     """ 404 status code error page """
     return render(request, '404.html', status=404)
 
 
-def error_403(request, exception):
-    """ 403  status code error page """
-    return render(request, '403.html')   
+def bad_request(request, exception):
+    """ 400 error page """
+    return render(request, '400.html')
 
-def my_custom_error_view(request, *args, **argv):
+
+def custom_error(request, *args, **argv):
     """ 500 status code error page """
     return render(request, '500.html', status=500)
-
-
