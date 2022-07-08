6f8613326e058ad3639e26ccfebcf4b213de5418
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-07-08 23:41:21 +0000

    install whitenoise

--- woofme/settings.py
+++ woofme/settings.py
@@ -66,6 +66,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 
 ]
 
