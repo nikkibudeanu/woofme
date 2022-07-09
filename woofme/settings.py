1c218ddd0f985489efa5abd62494aad29f79d78d
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-07-09 08:52:56 +0000

    delete whitenose

--- woofme/settings.py
+++ woofme/settings.py
@@ -63,9 +63,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-
-
 ]
 
 ROOT_URLCONF = 'woofme.urls'
